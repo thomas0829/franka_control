51c5418779569137d0e9d1b56e68a46a4ac12045
memmelma <marius.memmel@gmx.net>
2024-01-22 09:16:26 -0800

    add RLDS data convert

---
+++ scripts/mod_RLDS.sh
@@ -0,0 +1,1 @@
+/home/weirdlab/miniforge3/envs/rlds_env/bin/python rlds_dataset_mod/modify_rlds_dataset.py --data_dir '../data/rlds' --dataset='pick_red' --mods=resize_and_jpeg_encode --target_dir='../data/rlds_mod'

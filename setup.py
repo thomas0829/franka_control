0e715d78ba274c611b5cb5a78428b9f3878b8aa9
JacobB33 <hockeyviolin@gmail.com>
2024-02-01 12:34:59 -0800

    updated setup.py to install oculus_reader

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_namespace_packages
-
+requirements = [
+        "oculus_reader @ git+https://github.com/rail-berkeley/oculus_reader.git"
+        ]
 setup(
     name='robot_test',
     version='0.1.0',    
     packages=find_namespace_packages(), 
     include_package_data = True,
+    install_requires=requirements
     #package_dir={'': 'src'},
 )

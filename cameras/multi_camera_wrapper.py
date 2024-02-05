c5226680380b17703cc0062ad466d0904a502570
memmelma <marius.memmel@gmx.net>
2024-02-05 10:32:21 -0800

    add zed camera

--- cameras/multi_camera_wrapper.py
+++ cameras/multi_camera_wrapper.py
@@ -1,35 +1,87 @@
 from cameras.camera_thread import CameraThread
-from cameras.cv2_camera import gather_cv2_cameras, CV2Camera
 import time
 import cv2
 
 class MultiCameraWrapper:
 
-	def __init__(self, specific_cameras=None, use_threads=True):
+	def __init__(self, specific_cameras=None, use_threads=True, type="realsense", num_expected_cameras=None):
+		"""
+		Multi Camera Wrapper that returns images in order of serial number
+		params:
+		specific_cameras: list of camera objects
+		use_threads: bool, whether to use threads for reading the cameras
+		type: str, type of camera to use. Options are 'realsense' and 'zed'
+		:num_expected_cameras: int, number of cameras expected. If none, won't have default cameras
+		"""
+		
+		self.specific_cameras = specific_cameras
+		self.use_threads = use_threads
+		self.type = type
+		self.num_expected_cameras = num_expected_cameras
+		self._reset_cameras()
+		self.num_cameras = len(self._all_cameras)
+		if self.num_expected_cameras is not None:
+			assert self.num_cameras == self.num_expected_cameras, f'Expected {self.num_expected_cameras} cameras, got {self.num_cameras}'
+
+
+	def _reset_cameras(self):
+		"""
+		Resets the cameras to the state they were in when the object was first created
+		"""
+		if hasattr(self, '_all_cameras'):
+			for camera in self._all_cameras:
+				try:
+					camera.disable_camera()
+				except RuntimeError as e:
+					# This means that the camera is not plugged in right now
+					continue
 		self._all_cameras = []
 
-		if specific_cameras is not None:
-			self._all_cameras.extend(specific_cameras)
-		
-		# # Hard Code indices to separate wrist
-		# # from 3P camera
-		# cam_fp = CV2Camera(cv2.VideoCapture(0))
-		# cam_tp = CV2Camera(cv2.VideoCapture(2))
-		# self._all_cameras.extend([cam_fp, cam_tp])
-		
-		if use_threads:
-			for i in range(len(self._all_cameras)):
-				self._all_cameras[i] = CameraThread(self._all_cameras[i])
+		if self.specific_cameras is not None:
+			self._all_cameras.extend(self.specific_cameras)
+		else:
+			if self.type == 'realsense':
+				from cameras.realsense_camera import gather_realsense_cameras
+				self._all_cameras.extend(gather_realsense_cameras())
+			elif self.type == 'zed':
+				from cameras.zed_camera import gather_zed_cameras
+				# time.sleep(1)
+				self._all_cameras.extend(gather_zed_cameras())
+			else: 
+				raise NotImplementedError('Type of camera not implemented. You requested no specific cameras with no type')
+			
+			assert hasattr(self, "num_cameras") or len(self._all_cameras) > 0, 'No cameras found'
+		self._sort_cams_by_serial()
+
+		if self.use_threads:
+			for i, camera in enumerate(self._all_cameras):
+				self._all_cameras[i] = CameraThread(camera)
 			time.sleep(1)
 	
+
 	def read_cameras(self):
-		all_frames = []
-		for camera in self._all_cameras:
-			curr_feed = camera.read_camera()
-			if curr_feed is not None:
-				all_frames.extend(curr_feed)
-		return all_frames
+		try:
+			all_frames = []
+			for camera in self._all_cameras:
+				curr_feed = camera.read_camera()
+				if curr_feed is not None:
+					all_frames.extend(curr_feed)
+			if len(all_frames) != self.num_cameras * 2:
+				raise RuntimeError(f'Number of cameras changed, expected {self.num_cameras}, got {len(all_frames)//2}')
+			return all_frames
+		
+		except RuntimeError as e:
+			print(f'Error reading cameras: {e}')
+			input('Press enter when cameras are fixed:\t')
+			self._reset_cameras()
+			return self.read_cameras()
 
 	def disable_cameras(self):
 		for camera in self._all_cameras:
 			camera.disable_camera()
+	
+	def _sort_cams_by_serial(self):
+		"""
+		Sorts the cameras by serial number
+		"""
+		self._all_cameras.sort(key=lambda x: x._serial_number)
--- cameras/zed_camera.py
+++ cameras/zed_camera.py
@@ -3,57 +3,199 @@
 import cv2
 import numpy as np
 import time
+from copy import deepcopy
 
-def gather_zed_cameras(max_ind=20):
+try:
+	import pyzed.sl as sl
+except ModuleNotFoundError:
+	print("WARNING: You have not setup the ZED cameras, and currently cannot use them")
+
+def time_ms():
+	return time.time_ns() // 1_000_000
+
+def gather_zed_cameras():
 	all_zed_cameras = []
-	for i in range(max_ind):
-		device = cv2.VideoCapture(i)
-		if device.read()[0]:
-			correct_w = int(device.get(3)) == 1344
-			correct_h = int(device.get(4)) == 376
-			if correct_w and correct_h:
-				camera = ZedCamera(device)
-				all_zed_cameras.append(camera)
-			else:
-				device.release()
+	try:
+		cameras = sl.Camera.get_device_list()
+	except NameError:
+		return []
+
+	for cam in cameras:
+		cam = ZedCamera(cam)
+		all_zed_cameras.append(cam)
+
 	return all_zed_cameras
 
+
 class ZedCamera:
-	def __init__(self, device):
-		# Set the video resolution to HD720 (2560*720)
-		device.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
-		device.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-		self._device = device
+	def __init__(self, camera):
 		self._serial_number = str(camera.serial_number)
+		self._current_params = dict(
+			depth_mode=sl.DEPTH_MODE.NEURAL,
+			# coordinate_units=sl.UNIT.METER, # force milimeters
+			depth_minimum_distance=0.1,
+			depth_stabilization=False,
+			camera_resolution=sl.RESOLUTION.HD720,
+			camera_fps=60,
+			camera_image_flip=sl.FLIP_MODE.OFF,
+		)
+		self._extriniscs = {}
+
+		self.depth = True
+		self.pointcloud = False
+
+		self._configure_camera()
+
+	def _configure_camera(self):
+		# Close Existing Camera #
+		self.disable_camera()
+
+		# Initialize Readers #
+		self._cam = sl.Camera()
+		self._sbs_img = sl.Mat()
+		self._left_img = sl.Mat()
+		self._right_img = sl.Mat()
+		self._left_depth = sl.Mat()
+		self._right_depth = sl.Mat()
+		self._left_pointcloud = sl.Mat()
+		self._right_pointcloud = sl.Mat()
+		self._runtime = sl.RuntimeParameters()
+
+		sl_params = sl.InitParameters(**self._current_params)
+		sl_params.set_from_serial_number(int(self._serial_number))
+		status = self._cam.open(sl_params)
+		self._cam.set_camera_settings(sl.VIDEO_SETTINGS.EXPOSURE, 50)
+		if status != sl.ERROR_CODE.SUCCESS:
+			raise RuntimeError("Camera Failed To Open")
+
+		self.latency = int(2.5 * (1e3 / sl_params.camera_fps))
+		calib_params = (
+			self._cam.get_camera_information().camera_configuration.calibration_parameters
+		)
+		self._intrinsics = {
+			self._serial_number
+			+ "_left": self._process_intrinsics(calib_params.left_cam),
+			self._serial_number
+			+ "_right": self._process_intrinsics(calib_params.right_cam),
+		}
+
+	def _process_intrinsics(self, params):
+		intrinsics = {}
+		intrinsics["cameraMatrix"] = np.array(
+			[[params.fx, 0, params.cx], [0, params.fy, params.cy], [0, 0, 1]]
+		)
+		intrinsics["distCoeffs"] = np.array(list(params.disto))
+		return intrinsics
+
+	def get_intrinsics(self):
+		return deepcopy(self._intrinsics)
 
 	def read_camera(self):
-		# Get a new frame from camera
-		retval, frame = self._device.read()
-		if not retval: return None
+		# Read Camera #
+		timestamp_dict = {self._serial_number + "_read_start": time_ms()}
+		err = self._cam.grab(self._runtime)
+		if err != sl.ERROR_CODE.SUCCESS:
+			return None
+		timestamp_dict[self._serial_number + "_read_end"] = time_ms()
 
-		# Extract left and right images from side-by-side
-		read_time = time.time()
-		left_img, right_img = np.split(frame, 2, axis=1)
+		# Benchmark Latency #
+		received_time = self._cam.get_timestamp(
+			sl.TIME_REFERENCE.IMAGE
+		).get_milliseconds()
+		timestamp_dict[self._serial_number + "_frame_received"] = received_time
+		timestamp_dict[self._serial_number + "_estimated_capture"] = (
+			received_time - self.latency
+		)
+
+		self._cam.retrieve_image(self._left_img, sl.VIEW.LEFT)
+		self._cam.retrieve_image(self._right_img, sl.VIEW.RIGHT)
+
+		left_img = deepcopy(self._left_img.get_data())
 		left_img = cv2.cvtColor(left_img, cv2.COLOR_BGR2RGB)
-		right_img = cv2.cvtColor(right_img, cv2.COLOR_BGR2RGB)
 
+		dict_1 = {
+			"array": left_img,
+			"shape": left_img.shape,
+			"type": "rgb",
+			"read_time": received_time,
+			"serial_number": self._serial_number + "/left",
+		}
 
+		# right_img = deepcopy(self._right_img.get_data())
+		# right_img = cv2.cvtColor(right_img, cv2.COLOR_BGR2RGB)
 
-			# 	self._cam.retrieve_measure(self._left_depth, sl.MEASURE.DEPTH, resolution=self.resolution)
-        # 	self._cam.retrieve_measure(self._right_depth, sl.MEASURE.DEPTH_RIGHT, resolution=self.resolution)
-        # 	data_dict['depth'] = {
-        # 		self.serial_number + '_left': self._left_depth.get_data().copy(),
-        # 		self.serial_number + '_right': self._right_depth.get_data().copy()}
-		
-		# left_img = cv2.resize(left_img, dsize=(128, 96), interpolation=cv2.INTER_AREA)
-		# right_img = cv2.resize(right_img, dsize=(128, 96), interpolation=cv2.INTER_AREA)
+		# dict_1 = {'array': right_img,  'shape': right_img.shape, 'type': 'rgb',
+		# 	'read_time': received_time, 'serial_number': self._serial_number + '/right'}
 
-		dict_1 = {'array': left_img, 'shape': left_img.shape, 'type': 'rgb',
-			'read_time': read_time, 'serial_number': self._serial_number + '/left'}
-		dict_2 = {'array': right_img,  'shape': right_img.shape, 'type': 'rgb',
-			'read_time': read_time, 'serial_number': self._serial_number + '/right'}
-		
+		if self.depth:
+			self._cam.retrieve_measure(self._left_depth, sl.MEASURE.DEPTH)
+			self._cam.retrieve_measure(self._right_depth, sl.MEASURE.DEPTH_RIGHT)
+
+			left_depth = deepcopy(self._left_depth.get_data())
+
+			dict_2 = {
+				"array": left_depth,
+				"shape": left_depth.shape,
+				"type": "rgb",
+				"read_time": received_time,
+				"serial_number": self._serial_number + "/left",
+			}
+
+			# right_depth = deepcopy(self._right_depth.get_data())
+
+			# dict_2 = {'array': right_depth,  'shape': right_depth.shape, 'type': 'rgb',
+			# 	'read_time': received_time, 'serial_number': self._serial_number + '/right'}
+
+		# if self.pointcloud:
+		#	 self._cam.retrieve_measure(self._left_pointcloud, sl.MEASURE.XYZRGBA)
+		#	 self._cam.retrieve_measure(self._right_pointcloud, sl.MEASURE.XYZRGBA_RIGHT)
+
+		#	 left_pointcloud = self._left_pointcloud.get_data().copy()
+		#	 right_pointcloud = self._right_pointcloud.get_data().copy()
+
+		#	 dict_3 = {
+		#		 "array": left_pointcloud,
+		#		 "shape": left_pointcloud.shape,
+		#		 "type": "pointcloud",
+		#		 "read_time": received_time,
+		#		 "serial_number": self._serial_number + "/left",
+		#	 }
+		#	 # dict_3 = {'array': right_pointcloud, 'shape': right_pointcloud.shape, 'type': 'pointcloud',
+		#	 # 'read_time': received_time, 'serial_number': self._serial_number + '/right'}
+
 		return [dict_1, dict_2]
 
 	def disable_camera(self):
-		self._device.release()
+		if hasattr(self, "_cam"):
+			self._current_params = None
+			self._cam.close()
+
+	# def read_camera(self):
+	# 	# Get a new frame from camera
+	# 	retval, frame = self._device.read()
+	# 	if not retval: return None
+
+	# 	# Extract left and right images from side-by-side
+	# 	read_time = time.time()
+	# 	left_img, right_img = np.split(frame, 2, axis=1)
+	# 	left_img = cv2.cvtColor(left_img, cv2.COLOR_BGR2RGB)
+	# 	right_img = cv2.cvtColor(right_img, cv2.COLOR_BGR2RGB)
+
+	# 		# 	self._cam.retrieve_measure(self._left_depth, sl.MEASURE.DEPTH, resolution=self.resolution)
+	# 	# 	self._cam.retrieve_measure(self._right_depth, sl.MEASURE.DEPTH_RIGHT, resolution=self.resolution)
+	# 	# 	data_dict['depth'] = {
+	# 	# 		self._serial_number + '_left': self._left_depth.get_data().copy(),
+	# 	# 		self._serial_number + '_right': self._right_depth.get_data().copy()}
+
+	# 	# left_img = cv2.resize(left_img, dsize=(128, 96), interpolation=cv2.INTER_AREA)
+	# 	# right_img = cv2.resize(right_img, dsize=(128, 96), interpolation=cv2.INTER_AREA)
+
+	# 	dict_1 = {'array': left_img, 'shape': left_img.shape, 'type': 'rgb',
+	# 		'read_time': read_time, 'serial_number': self._serial_number + '/left'}
+	# 	dict_2 = {'array': right_img,  'shape': right_img.shape, 'type': 'rgb',
+	# 		'read_time': read_time, 'serial_number': self._serial_number + '/right'}
+
+	# 	return [dict_1, dict_2]
+
+	# def disable_camera(self):
+	# 	self._device.release()

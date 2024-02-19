68f4516c49d1761c0dd2d074002bc920cb1cd9c9
memmelma <marius.memmel@gmx.net>
2024-02-19 08:41:06 -0800

    change package name

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import find_namespace_packages, setup
+
 requirements = [
         "oculus_reader @ git+https://github.com/rail-berkeley/oculus_reader.git"
         ]
 setup(
-    name='robot_test',
+    name='weird_franka',
     version='0.1.0',    
     packages=find_namespace_packages(), 
     include_package_data = True,

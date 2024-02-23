c40d4194899e80307e5ed5b1ecd8bea36b3511c0
memmelma <marius.memmel@gmx.net>
2024-02-23 11:14:45 -0800

    ckpt seems to work

--- tests/test_mp.py
+++ tests/test_mp.py
@@ -7,14 +7,20 @@ import torch
 from robot.sim.vec_env.vec_env import make_env
 from utils.experiment import hydra_to_dict
 from utils.transformations import euler_to_quat, quat_to_euler
-from utils.transformations_mujoco import euler_to_quat_mujoco
+from utils.transformations_mujoco import *
 
+def wrap_angle(angle_diff):
+    return (angle_diff + np.pi) % (2 * np.pi) - np.pi
+
+def scale_angles(euler):
+    return np.arctan2(np.sin(euler), np.cos(euler))
 
 class PDController:
     def __init__(self, Kp, Kd, control_hz=10):
         self.Kp = Kp  # Proportional gain
         self.Kd = Kd  # Derivative gain
-        self.prev_error = 0
+        self.pos_prev_error = 0
+        self.quat_prev_error = 0
         self.dt = 1/control_hz
 
     def update(self, curr, des):
@@ -28,17 +34,36 @@ class PDController:
         Returns:
             float: The control output.
         """
-        # Calculate the error
-        error = des - curr
+        # Calculate the position error
+        pos_error = des[:3] - curr[:3]
 
-        # Calculate the derivative of the error
-        de = (error - self.prev_error) / self.dt
+        # Calculate the derivative of the position error
+        pos_error_dot = (pos_error - self.pos_prev_error) / self.dt
 
-        # Update the previous error and time for the next iteration
-        self.prev_error = error
+        # Update the previous position error and time for the next iteration
+        self.pos_prev_error = pos_error
 
-        # Calculate the control output
-        u = self.Kp * error + self.Kd * de
+        # Calculate the position control output
+        u_pos = self.Kp * pos_error + self.Kd * pos_error_dot
+
+
+        # Calculate the quaternion error
+        # quat_error = quat_difference_mujoco(des[3:], curr[3:])
+        quat_error = des[3:] - curr[3:]
+        quat_error = np.arctan2(np.sin(quat_error), np.cos(quat_error))
+
+        # Calculate the derivative of the quaternion error
+        quat_error_dot = (quat_error - self.quat_prev_error) / self.dt
+
+        # Update the previous quaternion error and time for the next iteration
+        self.quat_prev_error = quat_error
+
+        # Calculate the quaternion control output
+        u_quat = self.Kp * quat_error + self.Kd * quat_error_dot
+        
+
+        # Combine the position and quaternion control outputs
+        u = np.concatenate((u_pos, u_quat))
 
         return u
 
@@ -47,7 +72,7 @@ class PDController:
 def run_experiment(cfg):
 
     cfg.robot.DoF = 6
-    cfg.robot.on_screen_rendering = False
+    cfg.robot.on_screen_rendering = True
     cfg.robot.gripper = True
 
     env = make_env(
@@ -57,19 +82,19 @@ def run_experiment(cfg):
         verbose=True,
     )
 
-    env.reset()
+    # env.reset()
 
-    from robot.controllers.motion_planner import MotionPlanner
-    motion_planner = MotionPlanner(device=torch.device("cuda:0"))
+    # from robot.controllers.motion_planner import MotionPlanner
+    # motion_planner = MotionPlanner(device=torch.device("cuda:0"))
 
-    qpos = env.unwrapped._robot.get_joint_positions()
-    start = qpos
+    # qpos = env.unwrapped._robot.get_joint_positions()
+    # start = qpos
     
-    ee_pose = env.unwrapped._robot.get_ee_pose()
-    goal = np.concatenate((ee_pose[:3], euler_to_quat_mujoco(ee_pose[3:])))
-    goal[0] -= 0.1
-    goal[1] += 0.2
-    goal[2] += 0.1
+    # ee_pose = env.unwrapped._robot.get_ee_pose()
+    # goal = np.concatenate((ee_pose[:3], euler_to_quat_mujoco(ee_pose[3:])))
+    # goal[0] -= 0.1
+    # goal[1] += 0.2
+    # goal[2] += 0.1
 
     # qpos_plan = motion_planner.plan_motion(start, goal)
 
@@ -79,23 +104,86 @@ def run_experiment(cfg):
     #     env.render()
     #     time.sleep(0.1)
 
-    # TODO debug this!
     # ee_pose_plan = motion_planner.plan_motion(start, goal, return_ee_pose=True)
 
     env.reset()
-    for i in range(len(plan.ee_position)):
-        ee_pose = np.concatenate((plan.ee_position[i].cpu().numpy(), euler_to_quat_mujoco(quat_to_euler(plan.ee_quaternion[i].cpu().numpy()))))
-        env.unwrapped._robot.update_command(ee_pose - env.unwrapped._robot.get_ee_pose(), action_space="cartesian_position", blocking=True)
-        env.render()
-        time.sleep(0.1)
 
-    start = time.time()
-    for i in range(15):
-        env.reset()
-        for i in range(5):
-            obs, reward, done, info = env.step(env.action_space.sample())
-            env.render()
-    print(time.time() - start)
+    curr_pose = env.unwrapped._robot.get_ee_pose()
+    # curr_pose[:3] += np.array([0., 0., 0.])
+    curr_pose[4] += 0.5
+    curr_pose[5] += 0.5
+    des_pose = np.concatenate((curr_pose[:3], curr_pose[3:]))
+    # des_pose = np.concatenate((curr_pose[:3], euler_to_quat_mujoco(curr_pose[3:])))
+
+    pd_euler = PDController(Kp=1., Kd=0.)
+
+    error = []
+    currs = []
+    dess = []
+
+    # curr = env.unwrapped._robot.get_ee_pose()
+    # curr[3:] = scale_angles(curr[3:])
+
+    # for i in range(len(ee_pose_plan.ee_position)):
+
+        # des_pos = ee_pose_plan.ee_position[i].cpu().numpy()
+        # # des_euler = scale_angles(quat_to_euler(ee_pose_plan.ee_quaternion[i].cpu().numpy()))
+        # des_quat = euler_to_quat_mujoco(quat_to_euler(ee_pose_plan.ee_quaternion[i].cpu().numpy()))
+        # des = np.concatenate((des_pos, des_quat))
+
+    for j in range(100):
+            
+        curr_pose = env.unwrapped._robot.get_ee_pose()
+        # curr_quat = euler_to_quat_mujoco(curr_pose[3:])
+        # curr[3:] = scale_angles(curr[3:])
+        # curr_pose = np.concatenate((curr_pose[:3], curr_quat))
+        
+        # currs.append(curr)
+        # dess.append(des)
+        
+        act = pd_euler.update(curr_pose, des_pose)
+        # act[3:] = wrap_angle(scale_angles(act[3:]))
+        # act_euler = quat_to_euler(act[3:])
+        # act = np.concatenate((act[:3], act_euler, np.zeros(1)))
+        act = np.concatenate((act, np.zeros(1)))
+        
+        obs, _, _, _ = env.step(act)
+        
+        curr_pose = env.unwrapped._robot.get_ee_pose()
+        # curr_quat = euler_to_quat_mujoco(curr_pose[3:])
+        # curr[3:] = scale_angles(curr[3:])
+        # curr_pose = np.concatenate((curr_pose[:3], curr_quat))
+        
+        err = np.linalg.norm(des_pose[3:]-curr_pose[3:])
+        # err = quat_difference_mujoco(curr_quat, des_pose[3:])
+        error.append(np.sum(err))
+
+        # if err < 5e-2:
+        #     continue
+        print(np.sum(err), act)
+        env.render()
+
+    import matplotlib.pyplot as plt
+    # plt.plot(currs)
+    # plt.plot(dess, linestyle="--")
+    plt.plot(error)
+    plt.show()
+
+
+    # env.reset()
+    # for i in range(len(plan.ee_position)):
+    #     ee_pose = np.concatenate((plan.ee_position[i].cpu().numpy(), euler_to_quat_mujoco(quat_to_euler(plan.ee_quaternion[i].cpu().numpy()))))
+    #     env.unwrapped._robot.update_command(ee_pose - env.unwrapped._robot.get_ee_pose(), action_space="cartesian_position", blocking=True)
+    #     env.render()
+    #     time.sleep(0.1)
+
+    # start = time.time()
+    # for i in range(15):
+    #     env.reset()
+    #     for i in range(5):
+    #         obs, reward, done, info = env.step(env.action_space.sample())
+    #         env.render()
+    # print(time.time() - start)
 
 
 if __name__ == "__main__":

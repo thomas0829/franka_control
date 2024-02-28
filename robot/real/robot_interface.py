b0aff6e1dee5bdc2eb98230d4fa92b95ca0f6f05
memmelma <marius.memmel@gmx.net>
2024-02-28 14:18:35 -0800

    add terminal uitls

--- robot/real/robot_interface.py
+++ robot/real/robot_interface.py
@@ -9,6 +9,31 @@ import torch
 from utils.transformations import *
 
 # from r2d2.misc.subprocess_utils import run_terminal_command, run_threaded_command
+import multiprocessing
+import subprocess
+import threading
+
+
+def run_terminal_command(command):
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True, executable="/bin/bash", encoding="utf8"
+    )
+    
+    return process
+
+
+def run_threaded_command(command, args=(), daemon=True):
+    thread = threading.Thread(target=command, args=args, daemon=daemon)
+    thread.start()
+
+    return thread
+
+
+def run_multiprocessed_command(command, args=()):
+    process = multiprocessing.Process(target=command, args=args)
+    process.start()
+
+    return process
 
 class RobotInterfaceServer:
     def __init__(self, ip_address=""):

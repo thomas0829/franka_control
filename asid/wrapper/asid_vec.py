67b7805db45e8308239578f96dcb93e4444791bd
memmelma <marius.memmel@gmx.net>
2024-02-29 13:11:48 -0800

    add octo and openrt inference

--- asid/wrapper/asid_vec.py
+++ asid/wrapper/asid_vec.py
@@ -6,7 +6,7 @@ from asid.wrapper.asid_wrapper import ASIDWrapper
 
 def make_env(
     robot_cfg_dict,
-    env_cfg_dict,
+    env_cfg_dict=None,
     asid_cfg_dict=None,
     seed=0,
     device_id=0,
@@ -15,35 +15,40 @@ def make_env(
 
     if verbose:
         print("robot config", robot_cfg_dict)
-        print("env config", env_cfg_dict)
+        if env_cfg_dict is not None:
+            print("env config", env_cfg_dict)
+            if asid_cfg_dict is not None:
+                print("asid config", asid_cfg_dict)
 
-    robot_cfg_dict["model_name"] = robot_cfg_dict["model_name"].replace(
-        "base", env_cfg_dict["obj_id"]
-    )
+    if env_cfg_dict is not None:
+        robot_cfg_dict["model_name"] = robot_cfg_dict["model_name"].replace(
+            "base", env_cfg_dict["obj_id"]
+        )
 
     env = RobotEnv(**robot_cfg_dict, device_id=device_id, verbose=verbose)
 
-    if robot_cfg_dict["ip_address"] is None:
-        from robot.sim.mujoco.obj_wrapper import ObjWrapper
+    if env_cfg_dict is not None:
+        if robot_cfg_dict["ip_address"] is None:
+            from robot.sim.mujoco.obj_wrapper import ObjWrapper
 
-        env = ObjWrapper(env, **env_cfg_dict, verbose=verbose)
-    else:
-        from robot.real.obj_tracker_wrapper import ObjectTrackerWrapper
+            env = ObjWrapper(env, **env_cfg_dict, verbose=verbose)
+        else:
+            from robot.real.obj_tracker_wrapper import ObjectTrackerWrapper
 
-        env = ObjectTrackerWrapper(env, **env_cfg_dict, verbose=verbose)
+            env = ObjectTrackerWrapper(env, **env_cfg_dict, verbose=verbose)
 
-    if asid_cfg_dict is not None:
-        env = ASIDWrapper(env, **asid_cfg_dict, verbose=verbose)
+        if asid_cfg_dict is not None:
+            env = ASIDWrapper(env, **asid_cfg_dict, verbose=verbose)
 
-        if asid_cfg_dict["reward"]:
-            env.create_exp_reward(
-                make_env,
-                robot_cfg_dict,
-                env_cfg_dict,
-                asid_cfg_dict,
-                seed=seed,
-                device_id=device_id,
-            )
+            if asid_cfg_dict["reward"]:
+                env.create_exp_reward(
+                    make_env,
+                    robot_cfg_dict,
+                    env_cfg_dict,
+                    asid_cfg_dict,
+                    seed=seed,
+                    device_id=device_id,
+                )
 
     env.seed(seed)
 
@@ -52,7 +57,7 @@ def make_env(
 
 def make_vec_env(
     robot_cfg_dict,
-    env_cfg_dict,
+    env_cfg_dict=None,
     asid_cfg_dict=None,
     num_workers=1,
     seed=0,

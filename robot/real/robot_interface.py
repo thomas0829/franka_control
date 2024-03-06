5d3149957d81213f442b11bd24048942d89f0541
memmelma <marius.memmel@gmx.net>
2024-03-06 08:59:01 -0800

    gripper moves slower now but doesn't crash

--- robot/real/robot_interface.py
+++ robot/real/robot_interface.py
@@ -143,14 +143,14 @@ class RobotInterfaceServer:
         # gripper crashes when running multiple grasp,grasp,grasp,... or ungrasp,ungrasp,ungrasp,... -> use flag
         if command > 0.0 and not self._grasping:
             self._gripper.grasp(
-                grasp_width=0.0, speed=0.5, force=5.0, blocking=blocking
+                grasp_width=0.0, speed=0.05, force=0.1, blocking=blocking
             )
             self._grasping = True
         elif command == 0.0 and self._grasping:
             self._gripper.grasp(
                 grasp_width=self._max_gripper_width,
-                speed=0.5,
-                force=5.0,
+                speed=0.05,
+                force=0.1,
                 blocking=blocking,
             )
             self._grasping = False

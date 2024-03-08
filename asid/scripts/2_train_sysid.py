13319f62e0d6deedd9cab90d3d007013fad8a010
memmelma <marius.memmel@gmx.net>
2024-03-08 10:23:11 -0800

    video + fixing certain values

--- asid/scripts/2_train_sysid.py
+++ asid/scripts/2_train_sysid.py
@@ -10,7 +10,7 @@ from asid.wrapper.asid_vec import make_env, make_vec_env
 from utils.experiment import hydra_to_dict, set_random_seed, setup_wandb
 from utils.logger import Video, configure_logger
 from utils.system import get_device, set_gpu_mode
-
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 
 @hydra.main(config_path="../configs/", config_name="sysid_rod_sim", version_base="1.1")
 def run_experiment(cfg):
@@ -28,14 +28,18 @@ def run_experiment(cfg):
     logdir = os.path.join(cfg.log.dir, cfg.exp_id, str(cfg.seed), "sysid")
     logger = configure_logger(logdir, cfg.log.format_strings)
 
+    cfg.env.obj_pos_noise = False
+    # cfg.robot.on_screen_rendering = True
     cfg.asid.obs_noise = 0.0
+    cfg.asid.reward = False
+
     # train env
     envs_sysid = make_vec_env(
         robot_cfg_dict=hydra_to_dict(cfg.robot),
         env_cfg_dict=hydra_to_dict(cfg.env),
         asid_cfg_dict=hydra_to_dict(cfg.asid),
         # only use special sysid env when running on real
-        sysid=False if cfg.robot.ip_address is None else True,
+        sysid=True, # False if cfg.robot.ip_address is None else True,
         num_workers=cfg.num_workers,
         seed=cfg.seed,
         device_id=0,
@@ -45,16 +49,18 @@ def run_experiment(cfg):
         os.path.join(cfg.log.dir, cfg.exp_id, str(cfg.seed), "explore", "rollout.pkl")
     )
 
-    # # convert angles to mujoco quaternion and
-    # set obj pose
+    # set obj pose and account for conversion errors
+    rollout["obs"][..., -5:-4] = 0.02
+    rollout["obs"][..., -4:-3] = 0.93937271
     obj_pose = rollout["obs"][0, ..., -7:].copy()
     obj_pose = np.repeat(obj_pose, cfg.num_workers, axis=0)
     envs_sysid.set_obj_pose(obj_pose)
 
     params_dim = envs_sysid.get_parameters()[0].shape[0]
 
-    imageio.mimsave(f"real.gif", np.stack(rollout["rgbd"][..., :3])[:, 0], duration=3)
-    video = np.transpose(rollout["rgbd"][..., :3], (1, 0, 4, 2, 3))
+    imageio.mimsave(os.path.join(logger.dir, "real.mp4"), np.stack(rollout["rgbd"][..., :3]))
+    
+    video = np.transpose(rollout["rgbd"][None, ..., :3], (1, 0, 4, 2, 3))
     logger.record(
         f"real/trajectory",
         Video(video, fps=20),
@@ -84,7 +90,7 @@ def run_experiment(cfg):
             # log
             logger=logger,
             save_interval=cfg.log.save_interval,
-            eval_interval=cfg.log.eval_interval,
+            # eval_interval=cfg.log.eval_interval,
             verbose=True,
         )
 

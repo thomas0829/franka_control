dd7b5e03188baf40ef856e099416ad20a98cefeb
memmelma <marius.memmel@gmx.net>
2024-03-08 10:23:24 -0800

    reset up after rollout

--- asid/scripts/1_run_exploration_state.py
+++ asid/scripts/1_run_exploration_state.py
@@ -1,5 +1,6 @@
 import os
 import time
+os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
 
 import hydra
 import joblib
@@ -44,15 +45,14 @@ def run_experiment(cfg):
     logdir = os.path.join(cfg.log.dir, cfg.exp_id, str(cfg.seed), "explore")
     logger = configure_logger(logdir, cfg.log.format_strings)
 
-    cfg.num_workers = 1
+    cfg.robot.max_path_length = 10
     cfg.asid.obs_noise = 0.0
 
     # real env
-    envs = make_vec_env(
+    envs = make_env(
         robot_cfg_dict=hydra_to_dict(cfg.robot),
         env_cfg_dict=hydra_to_dict(cfg.env),
         asid_cfg_dict=hydra_to_dict(cfg.asid) if cfg.robot.ip_address is None else None,
-        num_workers=cfg.num_workers,
         seed=cfg.seed,
         device_id=0,
         verbose=False,
@@ -83,24 +83,40 @@ def run_experiment(cfg):
     #     env.set_parameters(param_ood)
 
     envs.seed(cfg.seed)
-    obs = envs.reset()
-
+    obs = envs.reset()[None]
+    
     done = False
     while not done:
-
+        
+        start = time.time()
         images_array = envs.render()
         data["rgbd"].append(images_array)
 
         act, _ = policy.predict(obs, deterministic=False)
-        next_obs, reward, done, info = envs.step(act)
+        next_obs, reward, done, info = envs.step(act[0])
 
+        print(f"Time: {time.time() - start}")
         print(
-            f"EE {np.around(obs[0,:2],3)} Obj {np.around(obs[0,11:13],3)} Act {np.around(act,3)}"
+            f"EE {np.around(obs[0,:2],3)} Obj {np.around(obs[0,11:13],3)} Act {np.around(act[0],3)}"
         )
 
         data["act"].append(act)
         data["obs"].append(obs)
-        obs = next_obs
+        obs = next_obs[None]
+
+    # reset up right
+    envs.unwrapped._reset_joint_qpos = np.array(
+            [
+                -0.13677763938903809,
+                0.006021707784384489,
+                -0.048125553876161575,
+                -2.0723488330841064,
+                -0.021774671971797943,
+                2.0718562602996826,
+                0.5588430762290955,
+            ]
+        )
+    envs.reset()
 
     if cfg.robot.ip_address is None:
         data["zeta"] = np.array(envs.get_parameters()[0])
@@ -110,16 +126,18 @@ def run_experiment(cfg):
 
     import imageio
 
-    imageio.mimwrite("explore.gif", data["rgbd"].squeeze(), duration=10)
+    imageio.mimwrite(os.path.join(logger.dir, "explore.mp4"), data["rgbd"].squeeze())
+    
     # b, t, c, h, w
-    video = np.transpose(data["rgbd"][..., :3], (1, 0, 4, 2, 3))
+    video = np.transpose(data["rgbd"][None, ..., :3], (1, 0, 4, 2, 3))
     logger.record(
         f"eval_policy/traj",
         Video(video, fps=10),
         exclude=["stdout"],
     )
     logger.dump(step=0)
-    filenames = joblib.dump(data, os.path.join(logdir, f"rollout.pkl"))
+    
+    filenames = joblib.dump(data, os.path.join(logger.dir, "rollout.pkl"))
     print("Saved rollout to", filenames)
 
 

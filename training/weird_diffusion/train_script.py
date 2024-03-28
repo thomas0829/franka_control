cb516bc1c8613c273936afc25376ac990a8ece50
memmelma <marius.memmel@gmx.net>
2024-03-28 15:32:30 -0700

    diffusion updates

--- training/weird_diffusion/train_script.py
+++ training/weird_diffusion/train_script.py
@@ -7,14 +7,15 @@ import torch
 import torch.nn as nn
 from tqdm.auto import tqdm
 
-from training.weird_diffusion.models.make_networks import \
-    instantiate_model_artifacts
+from training.weird_diffusion.models.make_networks import instantiate_model_artifacts
 from utils.experiment import hydra_to_dict, set_random_seed, setup_wandb
 from utils.logger import Video, configure_logger
 from utils.system import get_device, set_gpu_mode
 
 
-@hydra.main(version_base=None, config_path="../../configs", config_name="diffusion_policy_sim")
+@hydra.main(
+    version_base=None, config_path="../../configs", config_name="diffusion_policy_sim"
+)
 def run_experiment(cfg):
 
     if "wandb" in cfg.log.format_strings:
@@ -29,32 +30,39 @@ def run_experiment(cfg):
     logdir = os.path.join(cfg.log.dir, cfg.exp_id, str(cfg.seed))
     logger = configure_logger(logdir, cfg.log.format_strings)
 
-    nets, ema, noise_scheduler, optimizer, lr_scheduler, dataloader, stats, device = instantiate_model_artifacts(cfg,
-                                                                                                                 model_only=False)
+    nets, ema, noise_scheduler, optimizer, lr_scheduler, dataloader, stats, device = (
+        instantiate_model_artifacts(cfg, model_only=False)
+    )
     vision_feature_dim = 512 * len(cfg.training.image_keys)
-    with tqdm(range(cfg.training.num_epochs), desc='Epoch', ) as tglobal:
+
+    with tqdm(
+        range(cfg.training.num_epochs),
+        desc="Epoch",
+    ) as tglobal:
         # epoch loop
         for epoch_idx in tglobal:
             epoch_loss = list()
             # batch loop
-            with tqdm(dataloader, desc='Batch', leave=False) as tepoch:
+            with tqdm(dataloader, desc="Batch", leave=False) as tepoch:
                 for nbatch in tepoch:
                     # data normalized in dataset
                     # device transfer
-                    nimage = nbatch['image'][:, :cfg.training.obs_horizon].to(device)
-                    naction = nbatch['action'].to(device)
+                    nimage = nbatch["image"][:, : cfg.training.obs_horizon].to(device)
+                    naction = nbatch["action"].to(device)
                     B = nimage.shape[0]
 
-                    image_features = nets['vision_encoder'](
-                        nimage.flatten(end_dim=2))
+                    image_features = nets["vision_encoder"](nimage.flatten(end_dim=2))
 
                     image_features = image_features.reshape(
-                        B, cfg.training.obs_horizon, vision_feature_dim)
+                        B, cfg.training.obs_horizon, vision_feature_dim
+                    )
                     # (B,obs_horizon,D)
                     # concatenate vision feature and low-dim obs
                     obs_features = image_features
                     if cfg.training.with_state:
-                        nagent_pos = nbatch['state'][:, :cfg.training.obs_horizon].to(device)
+                        nagent_pos = nbatch["state"][:, : cfg.training.obs_horizon].to(
+                            device
+                        )
                         obs_features = torch.cat([image_features, nagent_pos], dim=-1)
 
                     obs_cond = obs_features.flatten(start_dim=1)
@@ -65,17 +73,19 @@ def run_experiment(cfg):
 
                     # sample a diffusion iteration for each data point
                     timesteps = torch.randint(
-                        0, noise_scheduler.config.num_train_timesteps,
-                        (B,), device=device
+                        0,
+                        noise_scheduler.config.num_train_timesteps,
+                        (B,),
+                        device=device,
                     ).long()
 
                     # add noise to the clean images according to the noise magnitude at each diffusion iteration
                     # (this is the forward diffusion process)
-                    noisy_actions = noise_scheduler.add_noise(
-                        naction, noise, timesteps)
+                    noisy_actions = noise_scheduler.add_noise(naction, noise, timesteps)
                     # predict the noise residual
-                    noise_pred = nets['noise_pred_net'](
-                        noisy_actions, timesteps, global_cond=obs_cond)
+                    noise_pred = nets["noise_pred_net"](
+                        noisy_actions, timesteps, global_cond=obs_cond
+                    )
 
                     # L2 loss
                     loss = nn.functional.mse_loss(noise_pred, noise)
@@ -99,19 +109,29 @@ def run_experiment(cfg):
             tglobal.set_postfix(loss=np.mean(epoch_loss))
 
             logger.record("mse_loss", np.mean(epoch_loss))
-            logger.record("learning_rate", optimizer.param_groups[0]['lr'])
-            
+            logger.record("learning_rate", optimizer.param_groups[0]["lr"])
+
             logger.dump(step=epoch_idx)
 
+            # Save checkpoint
+            # Create a state dict with the paramaters of the model, and the stats of the dataset
+            if epoch_idx % 10 == 0:
+                ema_nets = nets
+                ema.copy_to(ema_nets.parameters())
+                checkpoint = {
+                    "state_dict": ema_nets.state_dict(),
+                    "stats": stats,
+                    "config": cfg,
+                }
+                torch.save(
+                    checkpoint, os.path.join(logdir, f"diffusion_policy_{epoch_idx}")
+                )
+
+    # Save checkpoint
     ema_nets = nets
     ema.copy_to(ema_nets.parameters())
-    # save checkpoint
     # Create a state dict with the paramaters of the model, and the stats of the dataset
-    checkpoint = {
-        'state_dict': ema_nets.state_dict(),
-        'stats': stats,
-        'config': cfg
-    }
+    checkpoint = {"state_dict": ema_nets.state_dict(), "stats": stats, "config": cfg}
     torch.save(checkpoint, os.path.join(logdir, "diffusion_policy"))
 
 
